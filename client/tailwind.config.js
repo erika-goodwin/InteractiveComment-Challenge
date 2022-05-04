0b67a5bb29775891b955cb32db8fb1eb9f1843f1
erika-goodwin <shellvoerika@gmail.com>
2022-05-03 20:42:40 -0700

    installed frontend setup except lexical

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "main-purple": "#5556B8",
+        "light-purple": "#F5F6F9",
+        "light-red": "#DD696C",
+        "background-white": "#F6F7FA",
+      },
+    },
+  },
+  plugins: [],
+}
