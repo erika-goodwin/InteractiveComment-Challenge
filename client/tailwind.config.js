c2505caf824ede23ccb0e6cdba8b1f2653ff2c25
Jeremi <Kelhen@users.noreply.github.com>
2022-06-11 16:00:58 +0900

    chore: bump tailwind and remove set color

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        "main-purple": "#5556B8",
-        "light-purple": "#F5F6F9",
-        "light-red": "#DD696C",
-        "background-white": "#F6F7FA",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
-}
+};
